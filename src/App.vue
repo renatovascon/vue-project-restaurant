e540bac083bcf11823c7b62480b4f8b05420216b
Renato Vasconcelos Coura Soares <renato@Renatos-MacBook-Pro.local>
2023-12-21 00:11:52 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <MyNavbar :logo="logo_src" :alt="app_name" />
+    <div>
+      <router-view></router-view>
+    </div>
+    <MyFooter />
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyNavbar from './components/Navbar'
+import MyFooter from './components/Footer'
 
 export default {
-  name: 'App',
+  data: function() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make Your Burger"
+    }
+  },
   components: {
-    HelloWorld
+    MyNavbar,
+    MyFooter
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  .main-container {
+    margin: 50px;
+    min-height: 250px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
 </style>
